c04a8784c769b0123e4bfd58e8b138bfa404af78
Kelvin <akpuluchibuike@gmail.com>
2022-06-24 20:58:54 -0500

    edited the server.ts and ready for submission -2

--- src/server.ts
+++ src/server.ts
@@ -40,7 +40,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 		try {
 			const image_file: string = await filterImageFromURL( image_url ) ;
 			
-			res.status(200).sendFile(image_file, await deleteLocalFiles([image_file])) // await deleteLocalFiles([image_file]));
+			res.status(200).sendFile(image_file, await deleteLocalFiles([image_file]));
 
 		} catch (error) {
 			return res.status(422).send("The image can't be downloaded. Try again.");
